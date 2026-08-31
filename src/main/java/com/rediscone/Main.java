47bc6d7fe385ce42919aaf4936fa48e14267e5d2
nandininaidu <nandini2005729@gmail.com>
2026-08-31 11:35:46 +0530

    feat: basic TCP server listening on 6379, echoes bytes

--- src/main/java/com/rediscone/Main.java
+++ src/main/java/com/rediscone/Main.java
@@ -1,7 +1,22 @@
 package com.rediscone;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("redis-clone booting");
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(6379);
+        System.out.println("listening on 6379");
+        Socket client = server.accept();
+        InputStream in = client.getInputStream();
+        OutputStream out = client.getOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = in.read(buffer)) != -1) {
+            out.write(buffer, 0, len);
+        }
     }
 }
